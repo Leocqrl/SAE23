3bc55d32134a043ac50ce2d02cedacf6ae5e13de
Selimmlk <seliimious@gmail.com>
2025-05-22 16:54:58 +0200

    fiche d'appel fonctionnel : en cours

--- Partie 1/appel.php
+++ Partie 1/appel.php
@@ -6,6 +6,37 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    5191981
+    <form method="POST">
+        <table>
+            <tr><td colspan='5'>
+                <select>
+                    <option value="choix">choisissez votre module</option>
+                    <optgroup label="Modules">
+                    <option value="R209">R209</option>
+                    <option value="SAE23">SAE23</option>
+                </select>
+            </td></tr>
+            <tr><th>Groupe</th><th>N°</th><th>Nom Prénom</th><th>ABJ</th><th>ABI</th></tr>
+            <?php
+               $bdd= new mysqli('localhost','root',"",'SAE23');
+               $sql="SELECT groupe,idEtudiant,nom,prenom FROM Etudiant;";
+               $resultat=$bdd->query($sql);
+
+                if (!empty($resultat)) {
+                    while ($row = $resultat->fetch_assoc()) {
+                        $nomprenom = $row['nom']. ' '. $row['prenom'];
+                        echo '<tr>';
+                        echo '<td>'. $row['groupe'] .'</td>';
+                        echo '<td>'. $row['idEtudiant'] .'</td>';
+                        echo '<td>'. $nomprenom .'</td>';
+                        echo "<td> <button onclick='PassageABJ()'></button></td>";
+                        echo "<td> <button onclick='PassageABI()'></button></td>";
+                        echo '</tr>';
+                    }
+                }
+            ?>
+
+        </table>
+    </form>
 </body>
 </html>
