a130bf8cc801eb41065330d6c6d07ced00b21444
Leocqrl <2810leo768@gmail.com>
2025-05-26 13:17:37 +0200

    update automatique

--- Partie 1/consultation.php
+++ Partie 1/consultation.php
@@ -39,34 +39,48 @@ if (((!empty($_POST['nom']) && !empty($_POST['prenom'])) || !empty($_POST['id'])
 }
 
 if (!empty($resultat)) {
-    $liste_etudiant = [];
-    echo "<table>";
-    echo "<tr><th>Nom</th><th>Prénom</th><th>ID</th><th>Date de Cours</th><th>Absence</th></tr>";
+    echo "<form method='POST' action='update.php'>";
+    $liste_etudiant=[];
     while ($row = $resultat->fetch_assoc()) {
-        if ($row['Absence']==1){
-            $abs = "ABJ";
-        } 
-        elseif ($row['Absence']==2){
-            $abs = "ABI";
-        } 
-        else {
-            $abs = "/";
-        }
-        echo "<tr>";
-        echo "<td>" . $row['nom'] . "</td>";
-        echo "<td>" . $row['prenom'] . "</td>";
-        echo "<td>" . $row['idEtudiant'] . "</td>";
-        echo "<td>" . $row['date_Cours'] . "</td>";
-        echo "<td>" . $abs . "</td>";
-        echo "</tr>";
         $liste_etudiant[]=[
-            'idEtudiant' => $row['idEtudiant'],
             'nom' => $row['nom'],
             'prenom' => $row['prenom'],
+            'idEtudiant' => $row['idEtudiant'],
             'date_Cours' => $row['date_Cours'],
-            'Absence' => $abs
+            'horaire' => $row['horaire'],
+            'Absence' => $row['Absence'],
+            'idAbsences' => $row['idAbsences']
         ];
     }
+    echo "<h3>".$liste_etudiant[0]['nom'].' '.$liste_etudiant[0]['prenom'].' N°'.$liste_etudiant[0]['idEtudiant']."</h3>";
+    echo "<table>";
+    echo "<tr><th>Date de Cours</th><th>Horaire</th><th>Absence</th></tr>";
+    $N=0;
+    foreach ($liste_etudiant as $row) {
+        $N++;
+        if ($row['Absence']==1){
+            $abs = "<select name='absence_".$N."'><option value='1'>ABJ</option><option value='0'>Présent</option><option value='2'>ABI</option></select>";
+        } 
+        elseif ($row['Absence']==2){
+            $abs = "<select name='absence_".$N."'><option value='2'>ABI</option><option value='0'>Présent</option><option value='1'>ABJ</option></select>";
+        } 
+        else {
+            $abs = "<select name='absence_".$N."'><option value='0'>Présent</option><option value='1'>ABJ</option><option value='2'>ABI</option></select>";
+        }
+        echo "<tr>";
+        echo "<input type='hidden' name='idAbsences_".$N."' value='".$row['idAbsences']."'>";
+        echo "<td>" . $row['date_Cours'] . "</td>";
+        echo "<td>" . $row['horaire'] . "</td>";
+        echo "<td>" . $abs . "</td>";
+        echo "</tr>";   
+    }
+    echo "<table>";
+    echo "<input type='hidden' name='N' value='".$N."'>";
+    echo "<input type='submit' value='Modifier'>";
+    echo "</form>";
+
+    
 }
+
 ?>
 
---
+++ Partie 1/update.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $bdd=new mysqli("localhost","root","root","SAE23");
+    $sql = [];
+    for ($i=1; $i <= $_POST['N']; $i++) {
+        $sql[] = ["UPDATE Absences SET Absence = '".$_POST["absence_".$i]."' WHERE idAbsences = '".$_POST['idAbsences_'.$i]."';"];
+    }
+    foreach($sql as $requete) {
+        $bdd->query($requete[0]);
+        }
+    header('Location: consultation.html');
+}
+
+?>
